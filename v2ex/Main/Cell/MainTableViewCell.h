77b25cd35f5464c3e58b044be13251ab8261f1ea
Chenzuo <chenzuo0730@outlook.com>
2016-07-01 08:30:23 +0800

    addMainTableViewCell

---
+++ v2ex/Main/Cell/MainTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewCell.h
+//  v2ex
+//
+//  Created by ChenZuo on 16/6/30.
+//  Copyright © 2016年 chenzuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewCell : UITableViewCell
+
+@end
---
+++ v2ex/Main/Cell/MainTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.m
+//  v2ex
+//
+//  Created by ChenZuo on 16/6/30.
+//  Copyright © 2016年 chenzuo. All rights reserved.
+//
+
+#import "MainTableViewCell.h"
+
+@implementation MainTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
